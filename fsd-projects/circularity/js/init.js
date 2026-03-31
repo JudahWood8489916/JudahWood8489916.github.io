8b3fc2ac1e00c807ec33e7a68794105b538d260c
JudahWood8489916 <jw8489916@lpssonline.com>
2026-03-31 14:20:52 +0000

    todo complete

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -15,83 +15,74 @@ var init = function (window) {
         window.opspark.game = {};
         var game = window.opspark.game;
         
-        ///////////////////
-        // PROGRAM SETUP //
-        ///////////////////
+        ////////////////////////////////////////////////////////////
+        ///////////////// PROGRAM SETUP ////////////////////////////
+        ////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables
-var circle
+        var circle
+        var circles = []
 
-var circles = [ ]
 
-
+      
         // TODO 2 : Create a function that draws a circle 
-        function drawCircle() {
-  circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
+        
+       function drawCircle(){
+        circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
 
               physikz.addRandomVelocity(circle, canvas, 2, 2);
 
               view.addChild(circle);
 
               circles.push(circle);
+       }
+
+        // TODO 3 / 7 : Call the drawCircle() function 
+drawCircle()
+
+drawCircle()
+
+drawCircle()
+
+drawCircle()
+
+drawCircle()
         
-
-        }
-
-
-        // TODO 3 : Call the drawCircle() function
-drawCircle()
-
-drawCircle()
-
-drawCircle()
-
-drawCircle()
-
-drawCircle()
-
-
-        // TODO 7 : Use a loop to create multiple circles
-drawCircle()
-
-drawCircle()
-
-drawCircle()
-
-drawCircle()
-
-drawCircle()
- for (var loopsCompleted = 0; loopsCompleted < 175; loopsCompleted++) {
-
-            drawCircle();
-
-        }
-
-
-        ///////////////////
-        // PROGRAM LOGIC //
-        ///////////////////
+        ////////////////////////////////////////////////////////////
+        ///////////////// PROGRAM LOGIC ////////////////////////////
+        ////////////////////////////////////////////////////////////
         
         /* 
-        This Function is called 60 times/second, producing 60 frames/second.
+        This Function is called 60 times/second producing 60 frames/second.
         In each frame, for every circle, it should redraw that circle
         and check to see if it has drifted off the screen.         
         */
         function update() {
-            // TODO 4 : Update the position of each circle using physikz.updatePosition()
- for (var i = 0; i < circles.length; i++) {
+            // TODO 4 : Update the circle's position //
+physikz.updatePosition(circles[ 0 ])
 
-               physikz.updatePosition(circles[i]);
+physikz.updatePosition(circles[ 1 ])
 
-               game.checkCirclePosition(circles[i]);
+physikz.updatePosition(circles[ 2 ])
+
+physikz.updatePosition(circles[ 3 ])
+
+physikz.updatePosition(circles[ 4 ])
+                      
+            // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
+game.checkCirclePosition(circles[ 0 ])
+
+game.checkCirclePosition(circles[ 1 ])
+
+game.checkCirclePosition(circles[ 2 ])
+
+game.checkCirclePosition(circles[ 3 ])
+
+game.checkCirclePosition(circles[ 4 ])
 
 
-            
-            // TODO 5 : Call game.checkCirclePosition() on your circles
-           game.checkCirclePosition(circles[ 0 ])
 
-
-            // TODO 8 / TODO 9 : Iterate over the array
+                       // TODO 9 : Iterate over the array
            
             
         }
@@ -101,13 +92,13 @@ drawCircle()
         Function. If that circle drifts off the screen, this Function should move
         it to the opposite side of the screen.
         */
+
         game.checkCirclePosition = function(circle) {
 
             // if the circle has gone past the RIGHT side of the screen then place it on the LEFT
-            if ( circle.x > canvas.width ) {
-                circle.x = 0;
-            }
-            
+                      if(circle.x > canvas.width){
+  circle.x = 0
+}
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
              else if (circle.x < 0) {
 
@@ -121,7 +112,8 @@ drawCircle()
 
                 circle.y = canvas.height;
 
-
+            }
+           
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
         }
         
@@ -141,9 +133,10 @@ drawCircle()
     }
 };
 
-// DO NOT REMOVE THIS CODE //////////////////////////////////////////////////////
+// DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
 if((typeof process !== 'undefined') &&
     (typeof process.versions.node !== 'undefined')) {
     // here, export any references you need for tests //
     module.exports = init;
 }
+
